1c16080b50a3f58e566da66add5d1ee6d29bebb9
Lucas L. Targa <lucastarga@alunos.utfpr.edu.br>
2024-06-16 19:17:26 -0300

    Change show_table_data to get_table_data

--- src/display_table.py
+++ src/display_table.py
@@ -1,15 +1,20 @@
 from tabulate import tabulate
 from sqlalchemy import text
 import pandas as pd
+import os
+
+from export_data import export_to_csv, export_to_json
 
 def display_table(dataframe):
     '''Displays table from a dataframe using "psql" tabulate library format.'''
     print(tabulate(dataframe, headers='keys', tablefmt='psql'))
 
-def show_table_data(engine, table, limit):
+def get_table_data(engine, table, limit):
         query = f"SELECT * FROM {table} LIMIT {limit}"
         with engine.connect() as connection:
             result = connection.execute(text(query))
             data = result.fetchall()
             columns = result.keys()
-            return pd.DataFrame(data, columns=columns)
+            df = pd.DataFrame(data, columns=columns)
+             
+            return df
