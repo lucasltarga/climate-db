08a4e825d313de124ab66731ea1e13585e68b5d1
Lucas L. Targa <lucastarga@alunos.utfpr.edu.br>
2024-06-16 18:53:36 -0300

    Add primary key information to the tree view

--- src/tree_view.py
+++ src/tree_view.py
@@ -2,13 +2,32 @@ from rich.tree import Tree
 from sqlalchemy import inspect
 
 def get_db_tree(engine):
+    '''
+    Uses the Tree class of rich module to generate a tree visualization of the database 
+    including tables, views and columns details like name, type and if it is a primary key.
+    
+    Structure example:
+    Table1: table_name
+        column1 (type(size)) PK
+
+        column2 (type(size))
+
+    Table2: table2_name
+        column3 (type(size)) PK
+
+        column4 (type(size))
+    '''
     inspector = inspect(engine)
     tree = Tree("Database Schema")
     
     for table_name in inspector.get_table_names():
+        primary_keys = inspector.get_pk_constraint(table_name)['constrained_columns']
+       
         table_tree = tree.add(f"Table: {table_name}")
         for column in inspector.get_columns(table_name):
             column_info = f"{column['name']} ({column['type']})"
+            if column['name'] in primary_keys:
+                column_info += " PK"
             table_tree.add(column_info)
     
     for view_name in inspector.get_view_names():
