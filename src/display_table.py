6e4a56ab6445460c0d288bd0e8ad49996d5d86e9
Lucas L. Targa <lucastarga@alunos.utfpr.edu.br>
2024-06-16 13:07:46 -0300

    docs: Add docstrings

--- src/display_table.py
+++ src/display_table.py
@@ -1,4 +1,7 @@
 from tabulate import tabulate
+from sqlalchemy import text
+import pandas as pd
 
 def display_table(dataframe):
+    '''Displays table from a dataframe using "psql" tabulate library format.'''
     print(tabulate(dataframe, headers='keys', tablefmt='psql'))
